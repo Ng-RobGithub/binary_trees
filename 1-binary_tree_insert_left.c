a3bd9f632af669ae36ba8c055ed7e964015269df
Ng-RobGithub <ngrob4real@gmail.com>
2023-11-02 15:21:47 -0700

    inserts a node as the left-child of another node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
@@ -6,36 +5,26 @@
  * @parent: A pointer to the node to insert the left-child in
  * @value: The value to store in the new node
  *
- * Return: If memory allocation fails or parent is NULL, return NULL.
- * Otherwise, return a pointer to the new node.
+ * Return: A pointer to the newly inserted node or NULL on failure
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node, *temp;
+	binary_tree_t *new_node;
 
-	if (parent == NULL)
+	if (!parent)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->right = NULL;
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+	}
 
-	if (parent->left != NULL)
-	{
-		temp = parent->left;
-		parent->left = new_node;
-		new_node->left = temp;
-		temp->parent = new_node;
-	}
-	else
-	{
-		parent->left = new_node;
-		new_node->left = NULL;
-	}
+	parent->left = new_node;
 
 	return (new_node);
 }
