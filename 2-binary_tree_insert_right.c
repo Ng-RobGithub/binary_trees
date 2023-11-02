8c91b5c6b0267e09b691fffc9fe272101c125128
Ng-RobGithub <ngrob4real@gmail.com>
2023-11-02 16:30:43 -0700

    inserts a node as the right-child of another node

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Inserts a node as the
+ * right-child of another node.
+ * @parent: A pointer to the node to insert the right-child in.
+ * @value: The value to store in the new node.
+ *
+ * Return: If parent is NULL or an error occurs - NULL.
+ * Otherwise - a pointer to the new node.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (!parent)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
+
+	if (parent->right)
+	{
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
+	}
+
+	parent->right = new_node;
+
+	return (new_node);
+}
